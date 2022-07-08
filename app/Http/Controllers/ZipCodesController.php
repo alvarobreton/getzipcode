d1f9df17b7d311a50db427ff69cef670f8e57819
Alvaro Breton <alvarolimadb@gmail.com>
2022-07-07 23:01:35 -0500

    add structure

--- app/Http/Controllers/ZipCodesController.php
+++ app/Http/Controllers/ZipCodesController.php
@@ -10,7 +10,7 @@ class ZipCodesController extends Controller
     public function get($cp = null)
     {
 
-       $result = ZipCodeMX::searchCode($cp);
+       $result = $this->structure(ZipCodeMX::searchCode($cp));
 
        if(empty($result))
        {
@@ -18,6 +18,38 @@ class ZipCodesController extends Controller
        }
 
        return response()->json($result, 200);
+    }
+
+    private function structure($result = null)
+    {
+        if(empty($result[0]->d_codigo))
+        {
+            return null;
+        }
+        
+        $data['zip_code'] = $result[0]->d_codigo;
+        $data['locality'] = strtoupper($result[0]->d_ciudad);
+        $data['federal_entity'] = array(
+            'key'   => $result[0]->c_estado,
+            'name'  => strtoupper($result[0]->d_estado),
+            'code'  => null
+        );
+        $data['settlements'] = array();
+
+        foreach ($result as $key => $value) {
+
+            $settlements = array(
+                'key'           => $value->c_mnpio,
+                'name'          => strtoupper($value->d_asenta),
+                'zone_type'     => strtoupper($value->id_asenta_cpcons),
+                'settlement_type' => array(
+                    'name' => strtoupper($value->d_tipo_asenta),
+                ),
+            );
+            array_push($data['settlements'],$settlements);
+        }
+
+        return $data;
     }
     
 }
--- app/Models/ZipCodeMX.php
+++ app/Models/ZipCodeMX.php
@@ -11,35 +11,6 @@ class ZipCodeMX extends Model
 
     public function searchCode($search = null)
     {
-        $result = ZipCodeMX::where('d_codigo', $search)->get();
-
-        if(empty($result[0]->d_codigo))
-        {
-            return null;
-        }
-        
-        $data['zip_code'] = $result[0]->d_codigo;
-        $data['locality'] = strtoupper($result[0]->d_ciudad);
-        $data['federal_entity'] = array(
-            'key'   => $result[0]->c_estado,
-            'name'  => strtoupper($result[0]->d_estado),
-            'code'  => null
-        );
-        $data['settlements'] = array();
-
-        foreach ($result as $key => $value) {
-
-            $settlements = array(
-                'key'           => $value->c_mnpio,
-                'name'          => strtoupper($value->d_asenta),
-                'zone_type'     => strtoupper($value->id_asenta_cpcons),
-                'settlement_type' => array(
-                    'name' => strtoupper($value->d_tipo_asenta),
-                ),
-            );
-            array_push($data['settlements'],$settlements);
-        }
-
-        return $data;
+        return $result = ZipCodeMX::where('d_codigo', $search)->get();
     }
 }
